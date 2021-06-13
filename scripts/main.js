4f4f587a7c0aeff4209542a99d263a24e120b8d8
Cosmin-Ariton <cosminiris12@gmail.com>
2021-06-13 12:08:45 +0200

    main-js-first

---
+++ scripts/main.js
@@ -0,0 +1,31 @@
+/* const root = new Vue({
+
+    el: '#app',
+    data: {
+        albums: [],
+        genres: [],
+        genreToSearch: 'all',
+        apiURL: 'http://localhost:8888/php-ajax-dischi/api.php'
+    },
+
+    methods: {
+        getAPI() {
+            axios.get(this.apiURL, {
+                    params: {
+                        genre: this.genreToSearch
+                    }
+                })
+                .then(res => {
+                    this.albums = res.data.albums;
+                    this.genres = res.data.genres;
+                    console.log(this.genres);
+                }).catch(err => {
+                    console.log(err);
+                })
+        }
+    },
+
+    created() {
+        this.getAPI();
+    }
+}); */
